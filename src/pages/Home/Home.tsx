a694ac831f588b1e2e642f9380137b03ad36dcbd
Wilairat115 <64011212194@msu.ac.th>
2024-12-01 12:29:26 +0700

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Layout from "../../components/Layouts/Layout";
+import "../../styles/HomeStyle.css";
+import Section1 from "./Section1";
+
+function Home() {
+  return (
+    <Layout>
+      {/* Home Section Hero Banner */}
+      <Section1/>
+    </Layout>
+
+  )
+}
+
+export default Home
