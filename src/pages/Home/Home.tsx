9f8afd3d89ac8e33ad202effd8f20b7f43d25975
wilairat1703 <wilairat.wkwork@gmail.com>
2024-12-03 08:55:49 +0700

    03122024-finish-testAccount

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Layout from "../../components/Layouts/Layout";
 import "../../styles/HomeStyle.css";
 import Section1 from "./Section1";
+import Section2 from "./Section2";
 
 function Home() {
   return (
     <Layout>
       {/* Home Section Hero Banner */}
       <Section1/>
+      <Section2/>
     </Layout>
 
   )
---
+++ src/pages/Home/Section2.tsx
@@ -0,0 +1,8 @@
+
+function Section2() {
+  return (
+    <div>Section2</div>
+  )
+}
+
+export default Section2
